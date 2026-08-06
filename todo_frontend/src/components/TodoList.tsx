eb974f3deb5a8f39d1668c6ec134bd86640a382b
RajdipChatterjee <rajdipchatterjee3000@gmail.com>
2026-08-06 20:03:49 +0530

    Converted TodoCard custom component to DataGrid React component

--- todo_frontend/src/components/TodoList.tsx
+++ todo_frontend/src/components/TodoList.tsx
@@ -1,5 +1,18 @@
-import { Text } from "@fluentui/react-components";
-import TodoCard from "./TodoCard";
+import {
+  DataGrid,
+  makeStyles,
+  type TableColumnDefinition,
+  TableCellLayout,
+  createTableColumn,
+  DataGridHeader,
+  DataGridRow,
+  DataGridHeaderCell,
+  DataGridBody,
+  DataGridCell,
+  type TableColumnId,
+  type DataGridCellFocusMode,
+} from "@fluentui/react-components";
+import { EditRegular, DeleteRegular } from "@fluentui/react-icons";
 import type { Todo } from "../models/Todo";
 
 interface TodoListProps {
@@ -8,7 +21,95 @@ interface TodoListProps {
   deleteTodo: (id: string) => Promise<void>;
 }
 
+const useClasses = makeStyles({
+  container: {
+    display: "flex",
+    gap: "5px",
+    fontSize: "25px",
+  },
+});
+
 function TodoList({ todos, toggleStatus, deleteTodo }: TodoListProps) {
+  const classes = useClasses();
+
+  const columns: TableColumnDefinition<Todo>[] = [
+    createTableColumn<Todo>({
+      columnId: "Task",
+      compare: (a, b) => {
+        return a.title.localeCompare(b.title);
+      },
+      renderHeaderCell: () => {
+        return "Task";
+      },
+      renderCell: (item) => {
+        return <TableCellLayout>{item.title}</TableCellLayout>;
+      },
+    }),
+    createTableColumn<Todo>({
+      columnId: "Description",
+      compare: (a, b) => {
+        return a.title.localeCompare(b.title);
+      },
+      renderHeaderCell: () => {
+        return "Description";
+      },
+      renderCell: (item) => {
+        return <TableCellLayout>{item.description}</TableCellLayout>;
+      },
+    }),
+    createTableColumn<Todo>({
+      columnId: "Status",
+      compare: (a, b) => {
+        return a.title.localeCompare(b.title);
+      },
+      renderHeaderCell: () => {
+        return "Status";
+      },
+      renderCell: (item) => {
+        return (
+          <TableCellLayout>
+            {item.isCompleted ? "Completed" : "Pending"}
+          </TableCellLayout>
+        );
+      },
+    }),
+    createTableColumn<Todo>({
+      columnId: "Created At",
+      compare: (a, b) => {
+        return a.title.localeCompare(b.title);
+      },
+      renderHeaderCell: () => {
+        return "Task";
+      },
+      renderCell: (item) => {
+        return <TableCellLayout>{item.title}</TableCellLayout>;
+      },
+    }),
+    createTableColumn<Todo>({
+      columnId: "Actions",
+      compare: (a, b) => {
+        return a.title.localeCompare(b.title);
+      },
+      renderHeaderCell: () => {
+        return "Actions";
+      },
+      renderCell: (item) => {
+        return (
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <EditRegular
+              className={classes.container}
+              onClick={() => handleToggleStatus(item.id)}
+            />
+            <DeleteRegular
+              className={classes.container}
+              onClick={() => handleDeleteClick(item.id)}
+            />
+          </div>
+        );
+      },
+    }),
+  ];
+
   async function handleDeleteClick(id: string) {
     await deleteTodo(id);
   }
@@ -17,30 +118,53 @@ function TodoList({ todos, toggleStatus, deleteTodo }: TodoListProps) {
     await toggleStatus(id);
   }
 
+  const getCellFocusMode = (columnId: TableColumnId): DataGridCellFocusMode => {
+    switch (columnId) {
+      case "singleAction":
+        return "none";
+      case "actions":
+        return "group";
+      default:
+        return "cell";
+    }
+  };
   return (
-    <div>
-      {todos.map((todo) => (
-        <TodoCard
-          key={todo.id}
-          todo={todo}
-          handleToggleStatus={() => handleToggleStatus(todo.id)}
-          handleDeleteClick={() => handleDeleteClick(todo.id)}
-        />
-      ))}
-
-      {todos.length === 0 && (
-        <Text
-          italic
-          style={{
-            marginTop: 24,
-            display: "block",
-            textAlign: "center",
+    <DataGrid
+      items={todos}
+      columns={columns}
+      selectionMode="multiselect"
+      getRowId={(item) => item.id}
+      focusMode="composite"
+      style={{ minWidth: "550px" }}
+    >
+      <DataGridHeader>
+        <DataGridRow
+          selectionCell={{
+            checkboxIndicator: { "aria-label": "Select all rows" },
           }}
         >
-          No todos yet.
-        </Text>
-      )}
-    </div>
+          {({ renderHeaderCell }) => (
+            <DataGridHeaderCell>{renderHeaderCell()}</DataGridHeaderCell>
+          )}
+        </DataGridRow>
+      </DataGridHeader>
+      <DataGridBody<Todo>>
+        {({ item, rowId }) => (
+          <DataGridRow<Todo>
+            key={rowId}
+            selectionCell={{
+              checkboxIndicator: { "aria-label": "Select row" },
+            }}
+          >
+            {({ renderCell, columnId }) => (
+              <DataGridCell focusMode={getCellFocusMode(columnId)}>
+                {renderCell(item)}
+              </DataGridCell>
+            )}
+          </DataGridRow>
+        )}
+      </DataGridBody>
+    </DataGrid>
   );
 }
 
